2b80faa0e2cb89f99e4f645a9fe0b2104ccf4afe
Fidel Gandewo <fidelgandewo@gmail.com>
2026-05-04 01:49:54 +0700

    Menambahkan API Film

--- TP_MODUL10_103022400129/obj/Debug/net10.0/TP_MODUL10_103022400129.AssemblyInfo.cs
+++ TP_MODUL10_103022400129/obj/Debug/net10.0/TP_MODUL10_103022400129.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL10_103022400129")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48b54eb076c48326cb042f20e409efbb2770efb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL10_103022400129")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL10_103022400129")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
